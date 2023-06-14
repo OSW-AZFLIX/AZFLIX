7511678f5e63885dbb5959f8adbc8b7f2c87fbeb
Bae Gi-Won <bgw1218@gmail.com>
2023-06-14 21:37:27 +0900

    edit signup #32

--- JS/signup.js
+++ JS/signup.js
@@ -1,5 +1,19 @@
-$(function (){
+$(document).ready(function (){
   
+  $("#signupBtn1").mouseenter(function () { 
+    $(this).addClass("highlight");
+  });
+  $("#signupBtn1").mouseleave(function () { 
+    $(this).removeClass("highlight");
+  });
+
+  $("#signupBtn2").mouseenter(function () { 
+    $(this).addClass("highlight");
+  });
+  $("#signupBtn2").mouseleave(function () { 
+    $(this).removeClass("highlight");
+  });
+
   $("#signupBtn1").click(function(){
     var input_id = $("#uid").val();
     var validateEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
