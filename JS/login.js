0d9cbed5c2ff510ee20cf446aedcb569df694cfd
Bae Gi-Won <bgw1218@gmail.com>
2023-06-15 01:46:37 +0900

    edit login algorithm #33

--- JS/login.js
+++ JS/login.js
@@ -8,16 +8,20 @@ $(document).ready(function (){
   });
 
   $("#loginBtn").click(function(){
-    var userdata = JSON.parse(JSON.stringify(Member));
     var id = $("#uid").val();
     var pwd = $("#upw").val(); 
+    var data= [];
     var search_fail = 1;
 
+  
     if((id == "") || (pwd == "")){
       alert("이메일 또는 비밀번호를 입력하세요.");
     } else {
-      for (var i = 0; i < userdata.members.length; i++) {
-        if (userdata.members[i].ID.match(new RegExp(id, "i")) && userdata.members[i].PW.match(new RegExp(pwd, "i"))) {
+      for (var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        var value = JSON.parse(localStorage.getItem(key));
+        
+        if (value[0] == id && value[1] == pwd) {
           search_fail = 0;
           location.replace("infopage_movie.html");
         }
